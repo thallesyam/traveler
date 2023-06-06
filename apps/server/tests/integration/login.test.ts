5a50b72b29c8c533a6c8a0909947820c96311c19
thallesyam <thallesyam@gmail.com>
2023-06-06 20:39:22 -0300

    test: creating the test error paths and ensuring that the user will not be able to enter with invalid email and/or password

--- apps/server/tests/integration/login.test.ts
+++ apps/server/tests/integration/login.test.ts
@@ -16,3 +16,45 @@ test("Deve realizar o login com sucesso", async () => {
   const { token } = await login.execute(input)
   expect(token).toBe("123")
 })
+
+test("Deve realizar o login com um email inválido", async () => {
+  const user = new User("Thalles Ian", "thallesyam@gmail.com", "admin@123")
+  const userRepository = new UserRepositoryMemory()
+  await userRepository.save(user)
+  const input = {
+    email: "thallesyam1@gmail.com",
+    password: user.password,
+  }
+  const login = new Login(userRepository)
+  expect(async () => await login.execute(input)).rejects.toThrowError(
+    new Error("Email ou senha inválido")
+  )
+})
+
+test("Deve realizar o login com uma senha inválida", async () => {
+  const user = new User("Thalles Ian", "thallesyam@gmail.com", "admin@123")
+  const userRepository = new UserRepositoryMemory()
+  await userRepository.save(user)
+  const input = {
+    email: user.email,
+    password: "admin@admin",
+  }
+  const login = new Login(userRepository)
+  expect(async () => await login.execute(input)).rejects.toThrowError(
+    new Error("Email ou senha inválido")
+  )
+})
+
+test("Deve realizar o login com uma senha inválida", async () => {
+  const user = new User("Thalles Ian", "thallesyam@gmail.com", "admin@123")
+  const userRepository = new UserRepositoryMemory()
+  await userRepository.save(user)
+  const input = {
+    email: "thallesyam1@gmail.com",
+    password: "admin@admin",
+  }
+  const login = new Login(userRepository)
+  expect(async () => await login.execute(input)).rejects.toThrowError(
+    new Error("Email ou senha inválido")
+  )
+})
