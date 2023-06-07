54ece7a7dd1bd65dd7bc39766a526e7004199287
thallesyam <thallesyam@gmail.com>
2023-06-07 17:23:19 -0300

    test: create test to save city usecase

---
+++ apps/server/tests/integration/save-city.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "vitest"
+import { CityRepositoryMemory } from "@/infra/repositories/memory"
+import { SaveCity } from "@/application/usecases/save-city"
+
+test("Deve criar uma cidade com sucesso", async () => {
+  const cityRepository = new CityRepositoryMemory()
+  const input = {
+    name: "Rio de Janeiro",
+    images: ["fake-image"],
+    description:
+      "O Rio de Janeiro é uma cidade deslumbrante com paisagens de tirar o fôlego.",
+  }
+  const saveCity = new SaveCity(cityRepository)
+  await saveCity.execute(input)
+  const cities = await cityRepository.findAll()
+  expect(cities).toHaveLength(1)
+})
