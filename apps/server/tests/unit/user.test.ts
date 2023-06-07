39bfff0a89a34c35cf2d9c37050da715c10d0cd6
thallesyam <thallesyam@gmail.com>
2023-06-06 21:20:53 -0300

    test: ensure that user should be an valid email to create account

--- apps/server/tests/unit/user.test.ts
+++ apps/server/tests/unit/user.test.ts
@@ -1,13 +1,30 @@
 import { expect, test } from "vitest"
 import User from "@/domain/entities/user"
+import Password from "@/domain/entities/password"
 
 test("Deve criar um usuário válido", async () => {
-  const user = new User("Thalles Ian", "thallesyam@gmail.com", "admin@123")
+  const user = new User(
+    "Thalles Ian",
+    "thallesyam@gmail.com",
+    new Password("admin@123", "salt")
+  )
   expect(user).toStrictEqual(user)
 })
 
+test("Deve tentar criar um usuário com faltando informação", async () => {
+  expect(
+    () =>
+      new User("", "thallesyam@gmail.com", new Password("admin@123", "salt"))
+  ).toThrowError(new Error("Insufficient information to create the user"))
+})
+
 test("Deve tentar criar um usuário com email inválido", async () => {
-  expect(() => new User("Thalles Ian", "", "admin@123")).toThrowError(
-    new Error("Insufficient information to create the user")
-  )
+  expect(
+    () =>
+      new User(
+        "Thalles Ian",
+        "thallesyam.com",
+        new Password("admin@123", "salt")
+      )
+  ).toThrowError(new Error("Invalid email"))
 })
