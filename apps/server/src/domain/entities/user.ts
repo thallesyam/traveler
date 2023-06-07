2fdc47f4eacc7ccd56dab66cb79401ae526dab98
thallesyam <thallesyam@gmail.com>
2023-06-06 21:21:10 -0300

    feat: ensure that user should be an valid email to create account

--- apps/server/src/domain/entities/user.ts
+++ apps/server/src/domain/entities/user.ts
@@ -8,6 +8,10 @@ export default class User {
     readonly email: string,
     readonly password: Password
   ) {
+    if (!this.isValidEmail(this.email)) {
+      throw new Error("Invalid email")
+    }
+
     if (!name || !email || !password || !this.createdAt) {
       throw new Error("Insufficient information to create the user")
     }
@@ -15,6 +19,12 @@ export default class User {
 
   static async create(name: string, email: string, password: string) {
     return new User(name, email, await Password.create(password, "salt"))
+  }
+
+  isValidEmail(email: string) {
+    return String(email)
+      .toLowerCase()
+      .match(/^[\w-]+(\.[\w-]+)*@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/)
   }
 
   async validatePassword(password: string) {
