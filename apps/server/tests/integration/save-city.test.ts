92e120d83915eb2fc648ce70ecb3e67e63071f1b
thallesyam <thallesyam@gmail.com>
2023-06-07 19:39:29 -0300

    test: implement duplicate name error to test case

--- apps/server/tests/integration/save-city.test.ts
+++ apps/server/tests/integration/save-city.test.ts
@@ -15,3 +15,18 @@ test("Deve criar uma cidade com sucesso", async () => {
   const cities = await cityRepository.findAll()
   expect(cities).toHaveLength(1)
 })
+
+test("Deve tentar criar uma cidade com nome repetido", async () => {
+  const cityRepository = new CityRepositoryMemory()
+  const input = {
+    name: "Rio de Janeiro",
+    images: ["fake-image"],
+    description:
+      "O Rio de Janeiro é uma cidade deslumbrante com paisagens de tirar o fôlego.",
+  }
+  const saveCity = new SaveCity(cityRepository)
+  await saveCity.execute(input)
+  expect(async () => await saveCity.execute(input)).rejects.toThrowError(
+    new Error("A city with that name already exists")
+  )
+})
