48a1d55692fdbabbaf021011eb400d1c6d92e97d
thallesyam <thallesyam@gmail.com>
2023-06-06 21:13:53 -0300

    refac: change env name

--- apps/server/tests/unit/password.test.ts
+++ apps/server/tests/unit/password.test.ts
@@ -1,11 +1,11 @@
 import { expect, test } from "vitest"
 import Password from "@/domain/entities/password"
 
-const PASSWORD_HAS =
+const PASSWORD_HASH =
   "f1fa680348802c16e610e0afa109ef9fd2ea21001bf0449ea4372229cee93a13c3eb08a30068a92b82d376d195f5ed4bebfd9b51413a0ae23dbb38da9141a4b4"
 
 test("Deve criar uma senha", async function () {
   const password = await Password.create("12345678", "salt")
-  expect(password.value).toBe(PASSWORD_HAS)
+  expect(password.value).toBe(PASSWORD_HASH)
   expect(password.salt).toBe("salt")
 })
