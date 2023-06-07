1f71328c12cec420c46afbed7370bef9767d76c8
thallesyam <thallesyam@gmail.com>
2023-06-07 19:26:14 -0300

    test: create test to get city by id usecase

---
+++ apps/server/tests/integration/get-city-by-id.test.ts
@@ -0,0 +1,38 @@
+import { expect, test } from "vitest"
+import { CityRepositoryMemory } from "@/infra/repositories/memory"
+import { SaveCity } from "@/application/usecases"
+import { GetCityById } from "@/application/usecases"
+
+test("Deve buscar uma cidade por id com sucesso", async () => {
+  const cityRepository = new CityRepositoryMemory()
+  const input = {
+    name: "Rio de Janeiro",
+    images: ["fake-image"],
+    description:
+      "O Rio de Janeiro é uma cidade deslumbrante com paisagens de tirar o fôlego.",
+  }
+  const saveCity = new SaveCity(cityRepository)
+  await saveCity.execute(input)
+  const city = await cityRepository.findByName(input.name)
+  const getCityById = new GetCityById(cityRepository)
+  const cityById = await getCityById.execute({ id: city?.id ?? "" })
+  expect(cityById.name).toStrictEqual(input.name)
+  expect(cityById.images).toStrictEqual(input.images)
+  expect(cityById.description).toStrictEqual(input.description)
+})
+
+test("Deve buscar uma cidade com id inexistente", async () => {
+  const cityRepository = new CityRepositoryMemory()
+  const input = {
+    name: "Rio de Janeiro",
+    images: ["fake-image"],
+    description:
+      "O Rio de Janeiro é uma cidade deslumbrante com paisagens de tirar o fôlego.",
+  }
+  const saveCity = new SaveCity(cityRepository)
+  await saveCity.execute(input)
+  const getCityById = new GetCityById(cityRepository)
+  expect(
+    async () => await getCityById.execute({ id: "1" })
+  ).rejects.toThrowError(new Error("City not found"))
+})
