fb0c661bf7962abed755ad9add98fa43be2befe4
thallesyam <thallesyam@gmail.com>
2023-06-13 18:55:54 -0300

    refac: updating city update to get 100% coverage

--- apps/server/tests/integration/update-city.test.ts
+++ apps/server/tests/integration/update-city.test.ts
@@ -28,7 +28,7 @@ test("Deve editar todos os dados de uma cidade com sucesso", async () => {
   expect(newCityData.description).toEqual(updatedCity.description)
 })
 
-test("Deve editar apenas um dos dados de uma cidade com sucesso", async () => {
+test("Deve editar apenas um dos dados de uma cidade com sucesso utilizando nome", async () => {
   const cityRepository = new CityRepositoryMemory()
   const input = {
     name: "Rio de Janeiro",
@@ -46,6 +46,47 @@ test("Deve editar apenas um dos dados de uma cidade com sucesso", async () => {
   await updateCity.execute({ id: city.getCityId(), data: newCityData })
   const updatedCity = await cityRepository.findById(city.getCityId())
   expect(newCityData.name).toEqual(updatedCity.name)
+})
+
+test("Deve editar apenas um dos dados de uma cidade com sucesso utilizando descrição", async () => {
+  const cityRepository = new CityRepositoryMemory()
+  const input = {
+    name: "Rio de Janeiro",
+    images: ["fake-image"],
+    description:
+      "O Rio de Janeiro é uma cidade deslumbrante com paisagens de tirar o fôlego.",
+  }
+  const saveCity = new SaveCity(cityRepository)
+  await saveCity.execute(input)
+  const city = (await cityRepository.findByName(input.name)) as City
+  const newCityData = {
+    description:
+      "São Paulo é uma cidade deslumbrante com paisagens de tirar o fôlego.",
+  }
+  const updateCity = new UpdateCity(cityRepository)
+  await updateCity.execute({ id: city.getCityId(), data: newCityData })
+  const updatedCity = await cityRepository.findById(city.getCityId())
+  expect(newCityData.description).toEqual(updatedCity.description)
+})
+
+test("Deve editar apenas um dos dados de uma cidade com sucesso utilizando imagens", async () => {
+  const cityRepository = new CityRepositoryMemory()
+  const input = {
+    name: "Rio de Janeiro",
+    images: ["fake-image"],
+    description:
+      "O Rio de Janeiro é uma cidade deslumbrante com paisagens de tirar o fôlego.",
+  }
+  const saveCity = new SaveCity(cityRepository)
+  await saveCity.execute(input)
+  const city = (await cityRepository.findByName(input.name)) as City
+  const newCityData = {
+    images: ["fake-image", "fake-image1"],
+  }
+  const updateCity = new UpdateCity(cityRepository)
+  await updateCity.execute({ id: city.getCityId(), data: newCityData })
+  const updatedCity = await cityRepository.findById(city.getCityId())
+  expect(newCityData.images).toEqual(updatedCity.images)
 })
 
 test("Deve tentar editar os dados de uma cidade com id errado", async () => {
