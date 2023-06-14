5c765c1372a5354fe6130dbcea896c3bb6dedff6
thallesyam <thallesyam@gmail.com>
2023-06-14 16:42:19 -0300

    refac: remove unnecessary method

--- apps/server/tests/unit/address.test.ts
+++ apps/server/tests/unit/address.test.ts
@@ -12,7 +12,7 @@ test("Deve criar um endereço válido", async () => {
   expect(address.cep).toEqual("08225260")
   expect(address.street).toEqual("Rua Francisco da cunha")
   expect(address.number).toEqual("533")
-  expect(address.getCoordinate()).toEqual({ lat: 10, long: 10 })
+  expect(address.coordinate).toEqual({ lat: 10, long: 10 })
 })
 
 test("Deve tentar criar um endereço faltando informações", async () => {
