8f584209b361227f0f6ea8f6563429ba91424d14
thallesyam <thallesyam@gmail.com>
2023-06-16 21:44:23 -0300

    test: ensure category will be create with a correctly data

---
+++ apps/server/tests/unit/category.test.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "vitest"
+import { Category } from "@/domain/entities"
+
+test("Deve criar uma categoria válida", async () => {
+  const category = new Category("Pontos Turísticos", "fake-images", [
+    { cityId: "fake-id", localId: "fake-id" },
+  ])
+  expect(category.name).toEqual("Pontos Turísticos")
+  expect(category.image).toEqual("fake-images")
+  expect(category.locals).toHaveLength(1)
+  expect(category.getCategoryId()).toBeTruthy()
+})
+
+test("Deve tentar criar um endereço faltando informações", async () => {
+  expect(() => new Category("Pontos Turísticos", "", [])).toThrow(
+    new Error("Insufficient information to create category")
+  )
+})
