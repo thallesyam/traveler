c509522190626b14eaeb2a5de991eaffb8055c5e
thallesyam <thallesyam@gmail.com>
2023-06-19 19:25:06 -0300

    test: ensure that category data will be updated correctly

---
+++ apps/server/tests/integration/update-category.test.ts
@@ -0,0 +1,98 @@
+import { expect, test } from "vitest"
+import { CategoryRepositoryMemory } from "@/infra/repositories/memory"
+import { SaveCategory, UpdateCategory } from "@/application/usecases"
+import { Category } from "@/domain/entities"
+
+test("Deve editar todos os dados de uma categoria com sucesso", async () => {
+  const categoryRepository = new CategoryRepositoryMemory()
+  const input = {
+    name: "Ponto turistico",
+    image: "fake-image",
+  }
+  const saveCategory = new SaveCategory(categoryRepository)
+  await saveCategory.execute(input)
+  const category = (await categoryRepository.findByName(input.name)) as Category
+  const newCategoryData = {
+    name: "Evento",
+    image: "fake-image-evento",
+  }
+  const updateCategory = new UpdateCategory(categoryRepository)
+  await updateCategory.execute({
+    id: category.getCategoryId(),
+    data: newCategoryData,
+  })
+  const updatedCategory = await categoryRepository.findById(
+    category.getCategoryId()
+  )
+  expect(newCategoryData.name).toEqual(updatedCategory.name)
+  expect(newCategoryData.image).toEqual(updatedCategory.image)
+})
+
+test("Deve editar apenas um dos dados de uma categoria com sucesso utilizando nome", async () => {
+  const categoryRepository = new CategoryRepositoryMemory()
+  const input = {
+    name: "Ponto turistico",
+    image: "fake-image",
+  }
+  const input1 = {
+    name: "Praias",
+    image: "fake-image",
+  }
+  const saveCategory = new SaveCategory(categoryRepository)
+  await saveCategory.execute(input)
+  await saveCategory.execute(input1)
+  const category = (await categoryRepository.findByName(input.name)) as Category
+  const newCategoryData = {
+    name: "Evento",
+  }
+  const updateCategory = new UpdateCategory(categoryRepository)
+  await updateCategory.execute({
+    id: category.getCategoryId(),
+    data: newCategoryData,
+  })
+  const updatedCategory = await categoryRepository.findById(
+    category.getCategoryId()
+  )
+  expect(newCategoryData.name).toEqual(updatedCategory.name)
+})
+
+test("Deve editar apenas um dos dados de uma categoria com sucesso utilizando imagem", async () => {
+  const categoryRepository = new CategoryRepositoryMemory()
+  const input = {
+    name: "Ponto turistico",
+    image: "fake-image",
+  }
+  const saveCategory = new SaveCategory(categoryRepository)
+  await saveCategory.execute(input)
+  const category = (await categoryRepository.findByName(input.name)) as Category
+  const newCategoryData = {
+    image: "fake-image1",
+  }
+  const updateCategory = new UpdateCategory(categoryRepository)
+  await updateCategory.execute({
+    id: category.getCategoryId(),
+    data: newCategoryData,
+  })
+  const updatedCategory = await categoryRepository.findById(
+    category.getCategoryId()
+  )
+  expect(newCategoryData.image).toEqual(updatedCategory.image)
+})
+
+test("Deve tentar editar os dados de uma categoria com id errado", async () => {
+  const categoryRepository = new CategoryRepositoryMemory()
+  const input = {
+    name: "Ponto turistico",
+    image: "fake-image",
+  }
+  const saveCategory = new SaveCategory(categoryRepository)
+  await saveCategory.execute(input)
+  const newCategoryData = {
+    name: "Evento",
+  }
+  const updateCategory = new UpdateCategory(categoryRepository)
+  expect(
+    async () =>
+      await updateCategory.execute({ id: "fakeId", data: newCategoryData })
+  ).rejects.toThrow(new Error("Category not found"))
+})
