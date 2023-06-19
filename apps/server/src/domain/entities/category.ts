65db635357be2528ca77f7849693f4056b51fe17
thallesyam <thallesyam@gmail.com>
2023-06-19 18:37:26 -0300

    refac: change locals in category entity to a private attribute

--- apps/server/src/domain/entities/category.ts
+++ apps/server/src/domain/entities/category.ts
@@ -2,15 +2,13 @@ import { randomUUID } from "crypto"
 
 export class Category {
   private id: string
+  private locals: { cityId: string; localId: string }[]
 
-  constructor(
-    readonly name: string,
-    readonly image: string,
-    readonly locals: { cityId: string; localId: string }[]
-  ) {
+  constructor(readonly name: string, readonly image: string) {
     this.id = this.generateCategoryId()
+    this.locals = []
 
-    if (!name || !image || !locals) {
+    if (!name || !image) {
       throw new Error("Insufficient information to create category")
     }
   }
@@ -22,5 +20,13 @@ export class Category {
 
   getCategoryId() {
     return this.id
+  }
+
+  getCategories() {
+    return this.locals
+  }
+
+  setCategory(localId: string, cityId: string) {
+    this.locals.push({ localId, cityId })
   }
 }
--- apps/server/tests/unit/category.test.ts
+++ apps/server/tests/unit/category.test.ts
@@ -2,17 +2,16 @@ import { expect, test } from "vitest"
 import { Category } from "@/domain/entities"
 
 test("Deve criar uma categoria válida", async () => {
-  const category = new Category("Pontos Turísticos", "fake-images", [
-    { cityId: "fake-id", localId: "fake-id" },
-  ])
+  const category = new Category("Pontos Turísticos", "fake-images")
   expect(category.name).toEqual("Pontos Turísticos")
   expect(category.image).toEqual("fake-images")
-  expect(category.locals).toHaveLength(1)
+  expect(category.setCategory("fake-id", "fake-id"))
+  expect(category.getCategories()).toHaveLength(1)
   expect(category.getCategoryId()).toBeTruthy()
 })
 
 test("Deve tentar criar um endereço faltando informações", async () => {
-  expect(() => new Category("Pontos Turísticos", "", [])).toThrow(
+  expect(() => new Category("Pontos Turísticos", "")).toThrow(
     new Error("Insufficient information to create category")
   )
 })
