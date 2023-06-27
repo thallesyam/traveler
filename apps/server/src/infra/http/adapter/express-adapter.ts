7fe4f85c04eee14a62737f865dd4e8f7ad764c43
thallesyam <thallesyam@gmail.com>
2023-06-27 08:18:11 -0300

    feat: create express adapter

---
+++ apps/server/src/infra/http/adapter/express-adapter.ts
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from "cors"
+import { HttpServer } from "@/infra/http/ports"
+
+export default class ExpressAdapter implements HttpServer {
+  app: any
+
+  constructor() {
+    this.app = express()
+    this.app.use(express.json())
+    this.app.use(cors())
+  }
+
+  listen(port: number): void {
+    this.app.listen(port)
+  }
+
+  on(method: string, url: string, callback: Function): void {
+    this.app[method](url, async (req: any, res: any) => {
+      const output = await callback(req.params, req.body)
+      res.json(output)
+    })
+  }
+}
